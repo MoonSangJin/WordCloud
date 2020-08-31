83f1c111f099c07c312dad67f7f22d27c28a0380
MoonSangJin <1128msj@naver.com>
2020-08-31 15:50:22 +0900

    resiter wordcloud service using react-create-app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('app')); //index.html의 app이라는 공간에다가 App 컴포넌트를 그리겠다.
